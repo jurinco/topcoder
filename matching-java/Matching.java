ba18688ccfd1b9cdf108b2d7c2eee8674dfb0b3c
Alexey Noskov <alexey.noskov@gmail.com>
2012-01-16 19:46:44 +0400

    First task

---
+++ matching-java/Matching.java
@@ -0,0 +1,30 @@
+public class Matching {
+
+	private static final String[][] features = new String[][] {
+			new String[] { "CIRCLE", "SQUIGGLE", "DIAMOND" },
+			new String[] { "RED", "BLUE", "GREEN" },
+			new String[] { "SOLID", "STRIPED", "EMPTY" },
+			new String[] { "ONE", "TWO", "THREE" } };
+
+	private String findMatch(String first, String second, int feature) {
+		if (first.equals(second))
+			return first;
+		else
+			for (String s : features[feature])
+				if (!s.equals(first) && !s.equals(second))
+					return s;
+
+		return null;
+	}
+
+	public String[] findMatch(String[] first, String[] second) {
+		String[] res = new String[4];
+
+		for (int i = 0; i < 4; ++i) {
+			res[i] = findMatch(first[i], second[i], i);
+		}
+
+		return res;
+	}
+
+}
---
+++ matching-java/MatchingTest.java
@@ -0,0 +1,58 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MatchingTest {
+
+    protected Matching solution;
+
+    @Before
+    public void setUp() {
+        solution = new Matching();
+    }
+
+    @Test(timeout = 2000)
+    public void testCase0() {
+        String[] first = new String[]{"DIAMOND", "BLUE", "SOLID", "ONE"};
+        String[] second = new String[]{"DIAMOND", "GREEN", "SOLID", "TWO"};
+
+        String[] expected = new String[]{"DIAMOND", "RED", "SOLID", "THREE"};
+        String[] actual = solution.findMatch(first, second);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test(timeout = 2000)
+    public void testCase1() {
+        String[] first = new String[]{"CIRCLE", "GREEN", "EMPTY", "TWO"};
+        String[] second = new String[]{"DIAMOND", "BLUE", "STRIPED", "ONE"};
+
+        String[] expected = new String[]{"SQUIGGLE", "RED", "SOLID", "THREE"};
+        String[] actual = solution.findMatch(first, second);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test(timeout = 2000)
+    public void testCase2() {
+        String[] first = new String[]{"DIAMOND", "RED", "SOLID", "ONE"};
+        String[] second = new String[]{"SQUIGGLE", "BLUE", "SOLID", "TWO"};
+
+        String[] expected = new String[]{"CIRCLE", "GREEN", "SOLID", "THREE"};
+        String[] actual = solution.findMatch(first, second);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test(timeout = 2000)
+    public void testCase3() {
+        String[] first = new String[]{"SQUIGGLE", "RED", "STRIPED", "ONE"};
+        String[] second = new String[]{"SQUIGGLE", "RED", "STRIPED", "ONE"};
+
+        String[] expected = new String[]{"SQUIGGLE", "RED", "STRIPED", "ONE"};
+        String[] actual = solution.findMatch(first, second);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+}
