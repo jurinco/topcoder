69e8d523cee8692f55846414f416e70d6479957e
Alexey Noskov <alexey.noskov@gmail.com>
2012-03-07 19:22:58 +0400

    SRM535D1-250

---
+++ foxandgcdlcm-java/FoxAndGCDLCM.java
@@ -0,0 +1,35 @@
+/*
+ * SRM535D1-250
+ * SOLVED
+ */
+
+import static java.lang.Math.*;
+
+public class FoxAndGCDLCM {
+
+	public long get(long G, long L) {
+		if (G < 0 || L < 0 || G == 0 || L % G != 0)
+			return -1;
+
+		long A = L / G;
+
+		for (int st = (int) sqrt(A); st >= 1; --st)
+			if (A % st == 0 && gcd(st, A / st) == 1) {
+				System.out.println(st * G + " " + (A / st) * G);
+				return (st * G + (A / st) * G);
+			}
+
+		return -1;
+	}
+
+	private long gcd(long A, long B) {
+		while (A > 0) {
+			long n = A;
+			A = B % A;
+			B = n;
+		}
+
+		return B;
+	}
+
+}
