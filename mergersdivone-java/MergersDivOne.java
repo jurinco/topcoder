163f12995fe21fa0b1b9f770167221b6ba9e2613
Alexey Noskov <alexey.noskov@gmail.com>
2012-03-07 18:44:43 +0400

    SRM526D1-250

---
+++ mergersdivone-java/MergersDivOne.java
@@ -0,0 +1,21 @@
+/*
+ * SRM526D1-250
+ * SOLVED
+ */
+
+import java.util.*;
+
+public class MergersDivOne {
+
+	public double findMaximum(int[] revenues) {
+		Arrays.sort(revenues);
+
+		double rev = revenues[0];
+
+		for (int i = 1; i < revenues.length; ++i)
+			rev = (rev + revenues[i]) / 2;
+
+		return rev;
+	}
+
+}
